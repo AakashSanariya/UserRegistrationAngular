fb764ab3b9ec65c32e5a38bcd0d43bc14ed1e78e
Akash_Sanariya <akash.sanariya@brainvire.com>
2019-09-20 18:49:45 +0530

    change ngDoChek to ngOnInit In AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit{
               private authService: AuthServiceService
   ){}
 
-  ngDoCheck(){
+  ngOnInit(){
     /*
      * For Timer Start
      * */
